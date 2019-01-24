63c453f2a6046a61d133fb6dd451200da7206c0a
paulocayres <paulo.cayres@gmail.com>
2019-01-24 06:23:19 -0200

    Projeto NestJS com HBS, Bootstrap e Sass

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { join } from 'path';
+import { AppModule } from './app.module';
+import * as sassMiddleware from 'node-sass-middleware';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useStaticAssets(join(__dirname, 'public'));
+  app.setBaseViewsDir(join(__dirname, 'views'));
+  app.setViewEngine('hbs');
+
+  app.use(require('node-sass-middleware')({
+    src: __dirname,
+    dest: __dirname,
+    debug: true,
+    outputStyle: 'compressed',
+  }));
+
+  await app.listen(3000);
+}
+bootstrap();
