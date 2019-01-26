06c78c5a324ae5b211ac56799e20e7f83fdaa27d
paulocayres <paulo.cayres@gmail.com>
2019-01-26 21:10:03 -0200

    Commit pra compartilhamento

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { join } from 'path';
+import * as path from 'path';
 import { AppModule } from './app.module';
 import * as sassMiddleware from 'node-sass-middleware';
 
+ 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useStaticAssets(join(__dirname, 'public'));
+  app.useStaticAssets(join(__dirname, 'images'));
   app.setBaseViewsDir(join(__dirname, 'views'));
   app.setViewEngine('hbs');
 
