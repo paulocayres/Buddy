dda4debca56e39ed1621eb5bbe05f4a6545e2683
paulocayres <paulo.cayres@gmail.com>
2019-01-27 18:34:42 -0200

    Configuração para as imagens e CSS finalizada

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { join } from 'path';
 import * as path from 'path';
 import { AppModule } from './app.module';
-import * as sassMiddleware from 'node-sass-middleware';
 
- 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useStaticAssets(join(__dirname, 'public'));
-  app.useStaticAssets(join(__dirname, 'images'));
+  app.useStaticAssets(path.join(__dirname, 'public'));
+  app.useStaticAssets(path.join(__dirname, 'public/css'));
+  app.useStaticAssets(path.join(__dirname, 'public/images'));
   app.setBaseViewsDir(join(__dirname, 'views'));
   app.setViewEngine('hbs');
 
   app.use(require('node-sass-middleware')({
-    src: __dirname,
-    dest: __dirname,
+    src:  __dirname + '/public/css',
+    dest: __dirname + '/public/css',
     debug: true,
     outputStyle: 'compressed',
   }));
