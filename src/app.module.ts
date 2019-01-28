05279b4064bcfb178812330020734f1416f22e42
paulocayres <paulo.cayres@gmail.com>
2019-01-28 21:48:13 -0200

    Conexão com banco de dados remoto

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { VisitorSchema } from './visitor.schema';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/buddy', {useNewUrlParser: true}),
+  imports: [MongooseModule.forRoot('mongodb://paulo.cayres:pccr0976@ds245277.mlab.com:45277/buddy', {useNewUrlParser: true}),
   MongooseModule.forFeature([{ name: 'Visitor', schema: VisitorSchema }])],
   controllers: [AppController],
   providers: [AppService],
