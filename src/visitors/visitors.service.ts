ed943d2f26b16ad5f8d0c7f9573f5e63533d3c88
paulocayres <paulo.cayres@gmail.com>
2019-02-05 01:32:50 -0200

    teste se entra no serviço

--- src/visitors/visitors.service.ts
+++ src/visitors/visitors.service.ts
@@ -21,6 +21,7 @@ export class VisitorsService {
     }
 
     async captcha(token: any): Promise<any> {
+      Logger.log('entro serviço');
       this.body = { secret: process.env.captcha, response: token};
       Logger.log(this.body);
       const response = await this.http.post('https://www.google.com/recaptcha/api/siteverify', this.body );
