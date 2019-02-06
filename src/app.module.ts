7deddd897257acbac246a6d4d20043aaf951286c
paulocayres <paulo.cayres@gmail.com>
2019-02-06 02:04:39 -0200

    corrigido endereço do mongodb de produção

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { VisitorsModule } from './visitors/visitors.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/buddy', {useNewUrlParser: true}),
+  imports: [MongooseModule.forRoot('mongodb://paulo.cayres:pccr0976@ds245277.mlab.com:45277/buddy', {useNewUrlParser: true}),
   // 'mongodb://paulo.cayres:pccr0976@ds245277.mlab.com:45277/buddy'
   // 'mongodb://localhost:27017/buddy'
   VisitorsModule],
